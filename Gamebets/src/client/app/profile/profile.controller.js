9a2ebbb9b6a0ad42f4a22d42a2a2d41893147cc7
ludogi <donatgirones@gmail.com>
2017-05-02 10:43:26 +0200

    New profile update

--- Gamebets/src/client/app/profile/profile.controller.js
+++ Gamebets/src/client/app/profile/profile.controller.js
@@ -10,14 +10,47 @@
   function ProfileController(logger, $scope, $translate) {
     var vm = this;
     vm.title = 'Profile';
+    vm.inputPw = 'password';
+    vm.datos = {
+      username: '',
+      email: '',
+      password: '',
+    };
+    vm.limpiarCampos = limpiarCampos;
 
     activate();
     changeLanguage();
+
+    function limpiarCampos() {
+      vm.datos.username = '';
+      vm.datos.email = '';
+      vm.datos.password = '';
+
+    }
 
     function changeLanguage() {
       $scope.changeLanguage = function(key) {
         $translate.use(key);
       };
+    }
+
+    function updateProfile(){
+      var dataUserJSON = JSON.stringify(vm.datos);
+      if ($scope.profileform.$valid) {
+        dataservice.signup(dataUserJSON).then(function(response) {
+          if (response === 'Usuario ya existe') {
+            logger.error(response);
+          } else if (response.email === vm.datos.email) {
+            cookiesService.SetCredentials(response);
+            logger.success('Usuario modificado con exito');
+            $state.go('profile');
+          } else {
+            logger.error(response);
+          }
+        });
+      } else {
+        logger.success('Datos incorrectos');
+      }
     }
 
     function activate() {
