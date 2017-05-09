2c988fae26a8cbf31c2828b36597807bb5d65591
ludogi <donatgirones@gmail.com>
2017-05-09 13:15:25 +0200

    Nueva traduccion

--- Gamebets/src/client/app/profile/i18nApp/app.js
+++ Gamebets/src/client/app/profile/i18nApp/app.js
@@ -13,17 +13,17 @@
 
       $translateProvider.translations('en', {
         h1_your_profile: 'Your Profile',
-        email: 'E-Mail',
-        id: 'Id',
-        username: 'Username',
-        avatar: 'Avatar',
+        email: 'E-Mail:',
+        id: 'Id:',
+        username: 'Username:',
+        avatar: 'Avatar:',
           });
       $translateProvider.translations('es', {
         h1_your_profile: 'Tu Perfil',
-        email: 'Correo Electronico',
-        id: 'Identificador',
-        username: 'Nombre del Usuario',
-        avatar: 'Icono',
+        email: 'Correo Electronico:',
+        id: 'Identificador:',
+        username: 'Nombre del Usuario:',
+        avatar: 'Icono:',
       });
 
       $translateProvider.useSanitizeValueStrategy('escape');
