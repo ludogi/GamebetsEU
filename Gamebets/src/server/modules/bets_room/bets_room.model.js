01ffa53374269adcc7f2f4eadb4c1b239d2e9bc7
ludogi <donatgirones@gmail.com>
2017-05-24 23:28:55 +0200

    Oculto signup si eres user

--- Gamebets/src/server/modules/bets_room/bets_room.model.js
+++ Gamebets/src/server/modules/bets_room/bets_room.model.js
@@ -16,4 +16,18 @@ modeloBet.insertBet = function(bet, callback) {
   }
 };
 
+modeloBet.insertGame = function(bet, callback) {
+  if (mysql.connection) {
+    let sql = "INSERT INTO bets (user, pending, datestart, datefinish, matches) VALUES ("+bet.user+", "+bet.pending+", "+bet.datestart+", "+bet.datefinish+", " +bet.matches+")";
+    console.log(sql);
+    mysql.connection.query(sql, function(err, rows) {
+      if (err) {
+        throw err;
+      } else {
+        callback(null, rows);
+      }
+    });
+  }
+};
+
 module.exports = modeloBet;
